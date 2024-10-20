fb5a96843d2693cb5fa02f2fead3ba0928906957
Dan <dans.channels.contact@gmail.com>
2024-10-20 22:02:01 +0200

    Update custom_classes_as_types.js

--- custom_classes_as_types.js
+++ custom_classes_as_types.js
@@ -1,6 +1,20 @@
 const Signal = {
   coercion: null
 };
+
+Object.defineProperty(Number.prototype, 'int', {
+  get() {
+    Signal.coercion = null;
+    return new Int(this?.v ?? this.valueOf());
+  }
+});
+Object.defineProperty(Number.prototype, 'float', {
+  get() {
+    Signal.coercion = null;
+    return new Float(this?.v ?? this.valueOf());
+  }
+});
+
 class Int extends Number {
   constructor(num) {
     num = num?.v ?? num;
@@ -15,7 +29,7 @@ class Int extends Number {
     if (oldName) {
       if (oldName !== Signal.coercion) {
         // throw TypeError(`Invalid merge of ${Signal.coercion} with ${oldName}`);
-        console.error(`Invalid merge of ${Signal.coercion} with ${oldName}`);
+        console.error(`Invalid merge of ${oldName} with ${Signal.coercion}`);
         Signal.coercion = null;
         return NaN;
       }
@@ -53,7 +67,7 @@ class Float extends Number {
     if (oldName) {
       if (oldName !== Signal.coercion) {
         // throw TypeError(`Invalid merge of ${Signal.coercion} with ${oldName}`);
-        console.error(`Invalid merge of ${Signal.coercion} with ${oldName}`);
+        console.error(`Invalid merge of ${oldName} with ${Signal.coercion}`);
         Signal.coercion = null;
         return NaN;
       }
@@ -76,3 +90,26 @@ class Float extends Number {
     return val;
   }
 }
+
+const test = new Int(6.3);
+const float = new Float(7.7);
+const int = new test.t(9.8); // converts number to a custom type
+const converted = new float.t(int); // converts custom type to a custom type
+const res = test + int + float._; // expression fails because of type conflict
+const res2 = int + int + int + test._;
+const res3 = int + 5 + int + int + int._;
+const res4 = 5 + float._;
+const isix = (6.7).int; // repackages a number to be of Int "type"
+const fsix = (6.7).float; // repackages a number to be of Float "type"
+const res5 = ((float / 2) * 7 + float).int; // works on expresions, eliminates the need for ._
+const res6 = float + 5; // sets up the next type to be a NaN, missing ._
+const res7 = int._ + 5;
+
+console.log(converted);
+console.log(res);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);
+console.log(res6);
+console.log(res7);
