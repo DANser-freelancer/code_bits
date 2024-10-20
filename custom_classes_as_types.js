8fe31dc86785cf02d94a5b9a5f4346dc4c6a4302
Dan <dans.channels.contact@gmail.com>
2024-10-20 19:22:30 +0200

    Update custom_classes_as_types.js

--- custom_classes_as_types.js
+++ custom_classes_as_types.js
@@ -76,22 +76,3 @@ class Float extends Number {
     return val;
   }
 }
-
-let test = new Int(6.3);
-const float = new Float(7.7);
-const int = new test.t(9.8); // converts number to a custom type
-const converted = new float.t(int); // converts custom type to a custom type
-const res = test + int + float._; // expression fails because of type conflict
-const res2 = int + int + int + test._;
-const res3 = int + 5 + int + int + int._;
-const res4 = 5 + float._;
-const res5 = float + 5; // sets up the next type to be a NaN, missing ._
-const res6 = int._ + 5;
-
-console.log(converted);
-console.log(res);
-console.log(res2);
-console.log(res3);
-console.log(res4);
-console.log(res5);
-console.log(res6);
