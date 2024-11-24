69f20845e5e1b89060b044a62ce2a5bb25c8b720
Dan <dans.channels.contact@gmail.com>
2024-11-24 20:51:34 +0100

    Update where.js

--- where.js
+++ where.js
@@ -1,21 +1,11 @@
+'use strict';
 const { log } = console;
 const guy = {
   name: 'Ben',
   year: 2002
 };
 
-function where(fn, params) {
-  return function (...args) {
-    for (const key in params) {
-      Object.defineProperty(params, key, {
-        get: params[key].bind(null, ...args)
-      });
-    }
-    return fn.call(params, ...args);
-  };
-}
-
-const isAdultComplex = function (person, date) {
+function isAdultImperative(person, date) {
   if (person == undefined || !Number.isFinite(date)) {
     throw new Error('invalid input!');
   }
@@ -25,31 +15,24 @@ const isAdultComplex = function (person, date) {
   } else {
     return false;
   }
-};
+}
 
-// !TODO put the .where() on the Function prototype and make it work
-const isAdultSimplified = where(
-  function (person, date) {
-    return this.validIn ? this.age >= 18 : this.error;
-  },
-  {
-    validIn(person, date) {
-      if (person == undefined || !Number.isFinite(date)) {
-        return false;
-      } else {
-        return true;
-      }
-    },
-    age(person, date) {
-      return date - person.year;
-    },
-    error() {
-      throw new Error('invalid input!');
-    }
+function isAdultDeclarative(person, date) {
+  // buisness logic
+  return validIn() ? ofAge() : error();
+
+  // application logic, specific instructions
+  function validIn() {
+    person == undefined || !Number.isFinite(date) ? false : true;
   }
-);
+  function error() {
+    throw new Error('invalid input!');
+  }
+  function ofAge() {
+    return date - person.year >= 18;
+  }
+}
 
-const bool = isAdultComplex(guy, 2024);
-const bool2 = isAdultSimplified(guy, 2024);
-
-log(bool, bool2);
+log(isAdultImperative(guy, 2024));
+log(isAdultDeclarative(guy, 2024));
+log(isAdultDeclarative(guy, 2009));
