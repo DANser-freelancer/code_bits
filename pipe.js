a75a8a2eb527a508529a2f33e6ecb16782516643
Dan <dans.channels.contact@gmail.com>
2024-12-13 11:41:21 +0100

    Add files via upload

---
+++ pipe.js
@@ -0,0 +1,31 @@
+const { log } = console;
+
+function number(cbk, ...args) {
+  if (typeof cbk != 'function') return number.value;
+  number.value = cbk(number.value, ...args);
+  return number;
+}
+number.value = 5;
+
+function add(a, b, c) {
+  return a + b + c;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+
+const result = number(add, 14, 1)(subtract, 9)(multiply, 3)((a) => {
+  log(`
+		I'm at the end of the train,
+		it's cars make no part of a chain.
+		Each rolls on it's own, 
+		but the order is known. 
+		`);
+  // heh, in other words there's no recursion
+  return a;
+})();
+
+log(result);
