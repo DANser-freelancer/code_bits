2f056dea288061decba3ece45d0bb7275a7b5296
Dan <dans.channels.contact@gmail.com>
2025-05-22 01:23:38 +0200

    Add files via upload

---
+++ instanceof-proxy.js
@@ -0,0 +1,11 @@
+Object.defineProperty(Symbol, 'isProxy', {
+  value: Symbol(
+    `The underlying object or handler specifies that you are going through a a Proxy`
+  ),
+});
+Object.defineProperty(Proxy, Symbol.hasInstance, {
+  value: function (inst) {
+    return inst[Symbol.isProxy] === true;
+  },
+});
+// now either declare [Symbol.isProxy] in the handler getter or on the target
